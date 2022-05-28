70639fb45205bfcf2a1808d19a100ae07ed62875
Angela Montes Prieto <anngylitha_27@hotmail.es>
2022-05-27 20:39:12 -0500

    Autómata diseñado para la secuencia 5,10,20,40,80,160 en binario.

---
+++ ProgramaAF.java
@@ -0,0 +1,66 @@
+public class ProgramaAF {
+
+    public static void main(String[] args) {
+        int []input={1,0,1};
+        String inicio="q0";
+        String fin="q3";
+        String actual=inicio;
+        boolean terminar=false;
+        int contador=0;
+        
+        while(terminar==false){
+			if(contador>input.length-1){
+              System.out.println("Validacion de la cadena terminada");
+              terminar=true;
+              break;
+          } 
+//ESTADO Q0          
+   		 	  if(actual=="q0"){  
+   		 	  	if(input[contador]==1 ){
+   		 	  		actual="q1";
+                    System.out.println("Cambio de Q0 a Q1 con "+input[contador]);
+   		 	  	}
+   		 	  	contador++;
+   		 	  	continue;
+   		 	  }
+//ESTADO Q1   		 	  
+   		 	 if(actual=="q1"){  
+   		 	  	if(input[contador]==0 ){
+   		 	  		actual="q2";
+                    System.out.println("Cambio de Q1 a Q2 con "+input[contador]);
+   		 	  	}
+   		 	  	contador++;
+   		 	  	continue;
+   		 	  }
+//ESTADO Q2 
+   		 	 if(actual=="q2"){  
+   		 	  	if(input[contador]==1 ){
+   		 	  		actual="q3";
+                    System.out.println("Cambio de Q2 a Q3 con "+input[contador]);
+   		 	  	}
+   		 	  	contador++;
+   		 	  	continue;
+   		 	  }
+//ESTADO Q3
+   		 	  if(input[contador]==0 && contador==input.length-1){
+   		 	  		actual="q3";
+                    System.out.println("Cambio de Q3 a Q3 con "+input[contador]);
+                    break;
+                    }if(input[contador]==0){
+                         actual="q3";
+                         System.out.println("Cambio de Q3 a Q3 con "+input[contador]);
+						 }else{
+                               System.out.println("Cadena NO valida");
+                               break;
+                               }
+                               contador++;
+                               continue;   		 	    		 	                	            	
+        }
+if(actual==fin){
+   System.out.println("Cadena VALIDA");
+   }else{
+   		 System.out.println("Cadena INVALIDA");
+   		 }        
+   
+	}
+}
